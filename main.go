5e7cc6fc97915fa063a60b165f3a92ce7f97c8c0
김윤민 <dbsalszz@naver.com>
2022-09-21 22:46:08 +0900

    대,소문자로 구성된 문자/ 변수와 상수, 축약형

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+var named string = "gg"
 
 func main() {
 	fmt.Println("Fuck the World")
+	// something.SayHello()
+	// something.sayBye()
+
+	// const name string = "ymk"
+	// name = "abcd"
+	// fmt.Println(name)
+
+	// var name string = "ymk"
+	name := "ymk"
+	name = "abcd"
+	fmt.Println(named)
+
 }
