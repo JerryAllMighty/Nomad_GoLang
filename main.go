a40216c51de4178e4fcf43ade8314daee299bfec
김윤민 <dbsalszz@naver.com>
2022-10-02 12:21:52 +0900

    함수 기능 (naked return, 다중 파라미터, defer)과 반복문

--- main.go
+++ main.go
@@ -1,13 +1,44 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-var named string = "gg"
+// func multiply(a int, b int) int {
+// 	return a * b
+// }
+
+// func lenAndUpper(name string) (int, string) {
+// 	return len(name), strings.ToUpper(name)
+// }
+
+// var named string = "gg"
+
+// func lenAndUpper(name string) (length int, upperCase string) {
+// 	defer fmt.Println("I'm done")
+// 	length = len(name)
+// 	upperCase = strings.ToUpper(name)
+// 	return
+// }
+
+// func repeatMe(words ...string) {
+// 	fmt.Println(words)
+// }
+
+func superAdd(numbers ...int) int {
+	total := 0
+	for i := 0; i < len(numbers); i++ {
+		fmt.Println(numbers[i])
+		total += numbers[i]
+	}
+	return total
+}
 
 func main() {
-	fmt.Println("Fuck the World")
+	result := superAdd(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+	print(result)
+	// repeatMe("java", "python", "javascript", "and then there's me")
+	// a, b := lenAndUpper("abcd")
+	// fmt.Println(a, b)
+	// fmt.Println(multiply(2, 2))
 	// something.SayHello()
 	// something.sayBye()
 
@@ -16,8 +47,10 @@ func main() {
 	// fmt.Println(name)
 
 	// var name string = "ymk"
-	name := "ymk"
-	name = "abcd"
-	fmt.Println(named)
+	// name := "ymk"
+	// name = "abcd"
+	// fmt.Println(named)
+	// totalLength, upperName := lenAndUpper("nico")
+	// fmt.Println(totalLength, upperName)
 
 }
