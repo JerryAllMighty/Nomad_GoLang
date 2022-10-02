75b46de61df080a13d7fd38d4fe9526cb0956be6
김윤민 <dbsalszz@naver.com>
2022-10-02 12:55:04 +0900

    switch 사용

--- main.go
+++ main.go
@@ -32,10 +32,13 @@ func superAdd(numbers ...int) int {
 	return total
 }
 func canIDrink(age int) bool {
-	if koreanAge := age + 2; koreanAge < 18 {
+	switch koreanAge := age + 2; koreanAge {
+	case 10:
 		return false
+	case 18:
+		return true
 	}
-	return true
+	return false
 }
 func main() {
 	fmt.Println(canIDrink(18))
